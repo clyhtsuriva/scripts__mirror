093fc7bfae47a3ee91c2658bac609f72764b4be2
clyhtsuriva <aimeric@adjutor.xyz>
2022-11-02 22:33:05 +0100

    Renaming backup_usb32 and update-script-repo

--- bin/update-scripts-repo.sh
+++ bin/update-scripts-repo.sh
@@ -5,3 +5,4 @@ SCRIPTS_REPO_PATH="$HOME/workbench/scripts/"
 
 cp -rv "$HOME/.local/usr/local/bin" "$SCRIPTS_REPO_PATH"
 cp -rv "$HOME/workbench/auto-void-packages" "$SCRIPTS_REPO_PATH"
+cp -rv "$HOME/workbench/ansible" "$SCRIPTS_REPO_PATH"
