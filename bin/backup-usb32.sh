b221812192308716c68846b4f7c818b792705743
clyhtsuriva <aimeric@adjutor.xyz>
2023-01-10 08:41:12 +0100

    Adding infos about rync's parameters

--- bin/backup-usb32.sh
+++ bin/backup-usb32.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 rsync -az -u -v /mnt/32 ~/Documents/32.bak/
+
+# --archive, -a            archive mode is -rlptgoD (no -A,-X,-U,-N,-H)
+# --compress, -z           compress file data during the transfer
+# --update, -u             skip files that are newer on the receiver
+# --verbose, -v            increase verbosity
