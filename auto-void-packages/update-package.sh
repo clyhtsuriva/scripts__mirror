222777852cc5ffa7067992d06feab3ef726a331d
clyhtsuriva <aimeric@adjutor.xyz>
2023-01-18 23:33:49 +0100

    Renamed arm6l to arm6l-musl

--- auto-void-packages/update-package.sh
+++ auto-void-packages/update-package.sh
@@ -31,7 +31,7 @@ xgensum -f -i "$PKG" ; xgensum -f -i "$PKG" || exit 1
 ARCHS_FILE="$HOME/workbench/auto-void-packages/architectures.txt"
 [ -f "$ARCHS_FILE" ] && rm -v "$ARCHS_FILE"
 ./xbps-src -a armv7l pkg "$PKG" && echo "  - armv7l" >> "$ARCHS_FILE"
-./xbps-src -a armv6l-musl pkg "$PKG" && echo "  - armv6l" >> "$ARCHS_FILE"
+./xbps-src -a armv6l-musl pkg "$PKG" && echo "  - armv6l-musl" >> "$ARCHS_FILE"
 ./xbps-src -a aarch64-musl pkg "$PKG" && echo "  - aarch64-musl" >> "$ARCHS_FILE"
 ./xbps-src pkg "$PKG" || exit 1
 
