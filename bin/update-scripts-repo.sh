f18e9b4ebce97e220f3197d35608366f66896064
clyhtsuriva <aimeric@adjutor.xyz>
2025-01-31 16:58:56 +0100

    Moving ansible stuff to homelab-iac repo.
    Also adding patch flag to git add to interactively choose what to
    commit.

--- bin/update-scripts-repo.sh
+++ bin/update-scripts-repo.sh
@@ -7,9 +7,8 @@ pushd "$SCRIPTS_REPO_PATH" || exit 1
 
 cp -rv "$HOME/.local/usr/local/bin" .
 cp -rv "$HOME/workbench/auto-void-packages" .
-cp -rv "$HOME/workbench/ansible" .
 
 git status | less
-git add . && git commit -v && git push -v --progress
+git add --patch . && git commit -v && git push -v --progress
 
 popd || exit 1
